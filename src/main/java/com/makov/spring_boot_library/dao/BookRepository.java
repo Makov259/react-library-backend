e703ab64e667be1fbdce3e6df4b7c68648f2b227
Makov259 <mak00v259@gmail.com>
2024-09-15 00:52:53 +0300

    initial commit

---
+++ src/main/java/com/makov/spring_boot_library/dao/BookRepository.java
@@ -0,0 +1,9 @@
+package com.makov.spring_boot_library.dao;
+import com.makov.spring_boot_library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+    public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
